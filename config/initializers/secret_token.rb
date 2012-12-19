15a404960b01c59b84245805ffe1da49e895a450
Grant Jones <grjones90@gmail.com>
2012-12-18 19:44:09 -0600

    Added Site files and created rails app 'portfolio'

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Portfolio::Application.config.secret_token = 'af5900614c4d09580f5a65710a344d6b1d98b829eac431a65b32fe91c4859ace3119e5c740ae2c5d00f66d06d6617cdce0d3879a78f4ece68c44bb9e912000a0'
